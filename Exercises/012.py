a527e5cb4fe4175d97322088f6eb15ac45973097
LeoAndAlicia <355186603@qq.com>
2019-03-02 22:17:50 +0800

    Update 011.py

--- Exercises/012.py
+++ Exercises/012.py
@@ -16,10 +16,10 @@ def by_score(t):
 
 L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
 def by_name(t):
-	return t[0]
+	return t[0].lower()
 
 def by_score(t):
-	return t[1]
+	return -t[1]
 
 
 # 测试:
