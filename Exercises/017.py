825051c63e1194fb02479beb42791f0aa19ada60
LeoAndAlicia <355186603@qq.com>
2019-03-03 23:18:33 +0800

    Update 017.py

--- Exercises/017.py
+++ Exercises/017.py
@@ -2,45 +2,15 @@
 # -*- coding: utf-8 -*-
 
 '''
-再思考一下能否写出一个@log的decorator，使它既支持：
 
-@log
-def f():
-    pass
-又支持：
-
-@log('execute')
-def f():
-    pass
 '''
 
 import functools
-def log(text):
-	def decorator(fn):
-		@functools.wraps(fn)
-		def wrapper(*args,**kw):
-			if isinstance(text,(int,str)):
-				print('%s call %s' % (text,fn.__name__))
-			else :
-				print('call %s' % fn.__name__)
-			return fn(*args,**kw)
-		return wrapper
-	return decorator if isinstance(text,(int,str)) else decorator(text)
+
+int2 = functools.partial(int, base=2)
+
+print('1000000 =', int2('1000000'))
+print('1010101 =', int2('1010101'))
 
 
 # 测试:
-@log
-def a():
-	print('a')
-
-@log('b')
-def b():
-	print('b')
-
-@log('c')
-def c():
-	print('c')
-
-a()
-b()
-c()
