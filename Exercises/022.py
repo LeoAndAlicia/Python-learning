dbc965d562221c41bdd9b7c1016fbb8a550f1348
LeoAndAlicia <355186603@qq.com>
2019-03-04 22:54:27 +0800

    Update 022.py

--- Exercises/022.py
+++ Exercises/022.py
@@ -2,34 +2,31 @@
 # -*- coding: utf-8 -*-
 
 '''
-
+为了统计学生人数，可以给Student类增加一个类属性，每创建一个实例，该属性自动增加：
 '''
 
-class MyObject(object):
+class Student(object):
+    count = 0
 
-    def __init__(self):
-        self.x = 9
-
-    def power(self):
-        return self.x * self.x
-
-obj = MyObject()
-
-print('hasattr(obj, \'x\') =', hasattr(obj, 'x')) # 有属性'x'吗？
-print('hasattr(obj, \'y\') =', hasattr(obj, 'y')) # 有属性'y'吗？
-setattr(obj, 'y', 19) # 设置一个属性'y'
-print('hasattr(obj, \'y\') =', hasattr(obj, 'y')) # 有属性'y'吗？
-print('getattr(obj, \'y\') =', getattr(obj, 'y')) # 获取属性'y'
-print('obj.y =', obj.y) # 获取属性'y'
-
-print('getattr(obj, \'z\') =',getattr(obj, 'z', 404)) # 获取属性'z'，如果不存在，返回默认值404
-
-f = getattr(obj, 'power') # 获取属性'power'
-print(f)
-print(f())
+    def __init__(self, name):
+        self.name = name
+        Student.count += 1
 
 
 
 
 
 # 测试:
+if Student.count != 0:
+    print('测试失败!')
+else:
+    bart = Student('Bart')
+    if Student.count != 1:
+        print('测试失败!')
+    else:
+        lisa = Student('Bart')
+        if Student.count != 2:
+            print('测试失败!')
+        else:
+            print('Students:', Student.count)
+            print('测试通过!')
