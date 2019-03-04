bbdccce885798df5e530dd5f92e3b14678d1b732
LeoAndAlicia <355186603@qq.com>
2019-03-04 22:09:42 +0800

    Update 020.py

--- Exercises/020.py
+++ Exercises/020.py
@@ -2,35 +2,22 @@
 # -*- coding: utf-8 -*-
 
 '''
-请把下面的Student对象的gender字段对外隐藏起来，
-用get_gender()和set_gender()代替，并检查参数有效性：
+
 '''
 
-class Student(object):
-    def __init__(self, name, gender):
-        self.name = name
-        self.__gender = gender
+# type()
 
-    def get_gender(self):
-        return self.__gender
+print('type(123) =', type(123))
+print('type(\'123\') =', type('123'))
+print('type(None) =', type(None))
+print('type(abs) =', type(abs))
 
-    def set_gender(self,gender):
-        if gender == 'male' or 'female':
-            self.__gender = gender
-        else:
-            raise ValueError('bad gender')
+import types
+
+print('type(\'abc\')==str?', type('abc')==str)
 
 
 
 
 
 # 测试:
-bart = Student('Bart', 'male')
-if bart.get_gender() != 'male':
-    print('测试失败!')
-else:
-    bart.set_gender('female')
-    if bart.get_gender() != 'female':
-        print('测试失败!')
-    else:
-        print('测试成功!')
