8e3fcd20e77ef09abe5b134e2b8a175d7ed674a6
LeoAndAlicia <355186603@qq.com>
2019-03-05 18:30:50 +0800

    Create 026.py

---
+++ Exercises/026.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+把Student的gender属性改造为枚举类型，可以避免使用字符串：
+'''
+
+from enum import Enum, unique
+class Gender(Enum):
+    Male = 0
+    Female = 1
+
+class Student(object):
+    def __init__(self, name, gender):
+        self.name = name
+        self.gender = gender
+
+
+
+
+# 测试:
+bart = Student('Bart', Gender.Male)
+if bart.gender == Gender.Male:
+    print('测试通过!')
+else:
+    print('测试失败!')
