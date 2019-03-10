a9ce2bfe221383d06806b6d36cf8f5295c12cc98
LeoAndAlicia <355186603@qq.com>
2019-03-10 21:07:55 +0800

    Update settings.py

--- djangostart/djangostart/settings.py
+++ djangostart/djangostart/settings.py
@@ -123,6 +123,4 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-os.path.join(BASE_DIR, 'static')
-    ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
