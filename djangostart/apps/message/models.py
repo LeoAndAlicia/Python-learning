dab359adb2ad1673594f227ca8d03a5e37105c52
LeoAndAlicia <355186603@qq.com>
2019-03-11 18:45:11 +0800

    Update models.py

--- djangostart/apps/message/models.py
+++ djangostart/apps/message/models.py
@@ -7,10 +7,12 @@ from django.db import models
 
 
 class UserMessage(models.Model):
-    name = models.CharField(max_length=20, verbose_name=u"用户名")
+    object_id = models.CharField(max_length=50, default="", primary_key=True, verbose_name=u"主键")
+    name = models.CharField(max_length=20, null=True, blank=True, default="", verbose_name=u"用户名")
     email = models.EmailField(verbose_name=u"邮箱")
     address = models.CharField(max_length=100, verbose_name=u"联系地址")
     message = models.CharField(max_length=500, verbose_name=u"留言信息")
 
     class Meta:
         verbose_name = u"用户留言信息"
+        verbose_name_plural = verbose_name
