7949db3e573ecc981667fc004a3bcb0b40cddaa3
LeoAndAlicia <355186603@qq.com>
2019-03-20 23:21:29 +0800

    update

--- MyMooc/MyMooc/settings.py
+++ MyMooc/MyMooc/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'MyMooc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mymooc',
+        'USER': 'root',
+        'PASSWORD': 'l1g2d3l1g2d31234',
+        'HOST': '127.0.0.1'
     }
 }
 
