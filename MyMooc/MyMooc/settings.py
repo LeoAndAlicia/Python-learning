a311d29c4905ec3368c20d9c2e9628cea064a88c
LeoAndAlicia <355186603@qq.com>
2019-03-25 16:55:53 +0800

    Update

--- MyMooc/MyMooc/settings.py
+++ MyMooc/MyMooc/settings.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Django settings for MyMooc project.
 
@@ -11,10 +12,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+import sys
+# 将apps加入搜索路径
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 注册APP
     'users',
     'courses',
     'organization',
