42bc8f0073a524b83650996c6cee338cfdeca06a
LeoAndAlicia <355186603@qq.com>
2019-03-27 22:16:57 +0800

    update

--- MyMooc/MyMooc/urls.py
+++ MyMooc/MyMooc/urls.py
@@ -20,8 +20,11 @@ from django.views.static import serve
 from .settings import MEDIA_ROOT
 import xadmin
 
+from users.views import user_login
+
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
-    url(r'^', TemplateView.as_view(template_name='index.html'), name='index'),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+    url(r'^login/', user_login, name='login'),
     url(r'^media/(?P<path>.*)$',  serve, {"document_root": MEDIA_ROOT}),
 ]
