f38c6cf7a6480589e2d12edc8764e11c84c42ccf
LeoAndAlicia <355186603@qq.com>
2019-06-26 20:30:57 +0800

    创建VSCode工作区

---
+++ Exercises/python.code/033.py
@@ -0,0 +1,17 @@
+"""
+寻找“完美数”
+"""
+import math
+def main():
+	for num in range(1,10000):
+		sum = 0
+		for factor in range(1, int(math.sqrt(num))+1):
+			if num % factor == 0:
+				sum += factor
+				if (factor > 1) and (num / factor != factor):
+					sum += num / factor
+		if num != 1 and sum == num:
+			print(num)
+
+if __name__ == "__main__":
+	main()
