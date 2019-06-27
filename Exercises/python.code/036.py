d339a1420e3a3b40380195293dfe92cdd3b71a9b
LeoAndAlicia <355186603@qq.com>
2019-06-27 22:57:22 +0800

    Update 036.py

--- Exercises/python.code/036.py
+++ Exercises/python.code/036.py
@@ -1,13 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """
-实现计算求最大公约数和最小公倍数的函数
+实现判断一个数是不是回文数的函数
 """
-def gcd(x, y):
-    if x > y:
-        x , y = y, x
-    for factor in range(x , 0 , -1):
-        if x % factor == 0 and y % factor == 0:
-            return factor
+def is_palindrome(num):
+    temp = num
+    total = 0
+    while temp > 0:
+        total = temp % 10 + total * 10
+        temp //= 10
+    return total == num
 
-
-def lcm(x, y):
-    return x * y // gcd(x, y)
+x = int(input('请输入:'))
+print(is_palindrome(x))
