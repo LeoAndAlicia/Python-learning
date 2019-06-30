51a18fdc232711156697f40df4d8c3898b9309e4
LeoAndAlicia <355186603@qq.com>
2019-07-01 00:03:08 +0800

    Update 041.py

--- Exercises/python.code/041.py
+++ Exercises/python.code/041.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-设计一个函数返回给定文件名的后缀名
+设计一个函数返回传入的列表中最大和第二大的元素的值
 """
-def get_suffix(filename, has_dot = False):
-    """
-    :param filename: 文件名
-    :param has_dot: 后缀名是否需要带点
-    """
-    pos = filename.rfind('.')
-    if 0 < pos < len(filename) - 1:
-        index = pos if has_dot == True else pos + 1
-        print(filename[index:])
-    else:
-        print('')
+def max2(x):
+    m1 ,m2 = (x[0], x[1]) if x[0] > x[1] else (x[1], x[0])
+    for index in range(2, len(x)):
+        if x[index] > m1:
+            m2 = m1
+            m1 = x[index]
+        else:
+            m2 = x[index]
+    print(m1, m2)
 
 if __name__ == '__main__':
-    get_suffix('abc.exe', True)
+    max2([1,2,3,4,5,7])
