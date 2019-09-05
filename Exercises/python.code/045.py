4f5df828e6d603807d09adf6ce009660916f9b0e
LeoAndAlicia <355186603@qq.com>
2019-09-06 01:11:51 +0800

    Update 045.py

--- Exercises/python.code/045.py
+++ Exercises/python.code/045.py
@@ -1,30 +1,48 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-约瑟夫环问题
-《幸运的基督徒》
-有15个基督徒和15个非基督徒在海上遇险，为了能让一部分人活下来不得不将其中15个人扔到海里面去，
-有个人想了个办法就是大家围成一个圈，由某个人开始从1报数，报到9的人就扔到海里面，
-他后面的人接着从1开始报数，报到9的人继续扔到海里面，直到扔掉15个人。
-由于上帝的保佑，15个基督徒都幸免于难，问这些人最开始是怎么站的，哪些位置是基督徒哪些位置是非基督徒。
+井字棋游戏
 """
 
+import os
+
+
+def print_board(board):
+    print(board['TL'] + '|' + board['TM'] + '|' + board['TR'])
+    print('-+-+-')
+    print(board['ML'] + '|' + board['MM'] + '|' + board['MR'])
+    print('-+-+-')
+    print(board['BL'] + '|' + board['BM'] + '|' + board['BR'])
 
 
 def main():
-    persons = [True]*30
-    number, counter, index = 0, 0, 0
-    while counter < 15:
-        if persons[index]:
-            number += 1
-            if number == 9:
-                persons[index] = False
+    init_board = {
+        'TL': ' ', 'TM': ' ', 'TR': ' ',
+        'ML': ' ', 'MM': ' ', 'MR': ' ',
+        'BL': ' ', 'BM': ' ', 'BR': ' '
+    }
+    begin = True
+    while begin:
+        curr_board = init_board.copy()
+        begin = False
+        turn = 'x'
+        counter = 0
+        os.system('clS')
+        print_board(curr_board)
+        while counter < 9:
+            move = input('轮到%s走棋, 请输入位置: ' % turn)
+            if curr_board[move] == ' ':
                 counter += 1
-                number = 0
-        index += 1
-        index %= 30
-    for person in persons:
-        print('教' if person else '非', end=' ')
+                curr_board[move] = turn
+                if turn == 'x':
+                    turn = 'o'
+                else:
+                    turn = 'x'
+            os.system('clS')
+            print_board(curr_board)
+        choice = input('再玩一局?(yes|no)')
+        begin = choice == 'yes'
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
