e3dce9fa050ef9234a7bcec74c5836bbd3b11a46
LeoAndAlicia <355186603@qq.com>
2019-09-05 21:12:39 +0800

    Update 043.py

--- Exercises/python.code/043.py
+++ Exercises/python.code/043.py
@@ -1,20 +1,38 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-打印杨辉三角
+双色球选号
 """
+from random import randrange, randint, sample
+
+
+def display(balls):
+    """
+    输出列表中的双色球号码
+    """
+    for index, ball in enumerate(balls):
+        if index == len(balls) - 1:
+            print('|', end=' ')
+        print('%02d' % ball, end=' ')
+    print()
+
+
+def random_select():
+    """
+    随机选择一组号码
+    """
+    red_balls = [x for x in range(1, 34)]
+    selected_balls = []
+    selected_balls = sample(red_balls, 6)
+    selected_balls.sort()
+    selected_balls.append(randint(1, 16))
+    return selected_balls
+
+
 def main():
-    num = int(input('Number of rows: '))
-    yh = [[]] * num
-    for row in range(len(yh)):
-        yh[row] = [None] * (row + 1)
-        for col in range(len(yh[row])):
-            if col == 0 or col == row:
-                yh[row][col] = 1
-            else:
-                yh[row][col] = yh[row - 1][col] + yh[row - 1][col - 1]
-            print(yh[row][col], end='\t')
-        print()
+    n = int(input('机选几注: '))
+    for _ in range(n):
+        display(random_select())
 
 
 if __name__ == '__main__':
